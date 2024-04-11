18d359c01f572a4f2ea4166e8552a7ce7ee17ca1
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-04-11 22:41:44 +0900

    Update Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,8 +3,11 @@ import type { StorybookConfig } from '@storybook/nextjs';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
-    '../src/pageContainer/**/*.stories.@(js|jsx|ts|tsx)',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
+    '@storybook/addon-styling-webpack',
   ],
   framework: {
     name: '@storybook/nextjs',
