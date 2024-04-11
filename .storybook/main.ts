0d9c2137b1744974ddcf28b69236af8fc284f360
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-04-11 13:52:19 +0900

    스토리북 세팅

---
+++ .storybook/main.ts
@@ -0,0 +1,18 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/pageContainer/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  staticDirs: ['../public'],
+};
+export default config;
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,9 @@
 'use client';
 
-const Button = () => <button>dd</button>;
+import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
+
+const Button: React.FC<
+  DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+> = ({ children }) => <button>{children}</button>;
 
 export default Button;
