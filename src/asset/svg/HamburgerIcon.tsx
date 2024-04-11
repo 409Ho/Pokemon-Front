b8070b09712129f05615af0649166f6aa7fba2d5
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-04-11 15:49:52 +0900

    Update HamburgerIcon

--- src/asset/svg/HamburgerIcon.tsx
+++ src/asset/svg/HamburgerIcon.tsx
@@ -3,8 +3,8 @@
 export const HamburgerIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="44"
-    height="44"
+    width="2.75rem"
+    height="2.75rem"
     viewBox="0 0 44 44"
     fill="none"
   >
@@ -12,8 +12,8 @@ export const HamburgerIcon = () => (
       d="M6.875 12.375H37.125M6.875 22H37.125M6.875 31.625H37.125"
       stroke="black"
       stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
