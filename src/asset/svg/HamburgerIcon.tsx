a4ae4cb21689259b051c7db6116c20d3dc235c60
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-04-11 15:49:43 +0900

    Add Hamburger Icon

---
+++ src/asset/svg/HamburgerIcon.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+export const HamburgerIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="44"
+    height="44"
+    viewBox="0 0 44 44"
+    fill="none"
+  >
+    <path
+      d="M6.875 12.375H37.125M6.875 22H37.125M6.875 31.625H37.125"
+      stroke="black"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+);
