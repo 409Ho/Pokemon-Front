f2dd4d7d890f711ed7f96030f1fba82e08729830
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-04-12 12:03:48 +0900

    Add SignInPage story

---
+++ src/pageContainer/SignInPage/style.ts
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled';
+
+export const SignInPageWrapper = styled.div`
+  margin: 0 auto;
+  width: 600px;
+  height: 100vh;
+  background-color: #ff0000;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ContentWrapper = styled.div`
+  flex: 1;
+  margin: 0 auto;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5625rem;
+  margin: 263px 0 21rem 0;
+`;
