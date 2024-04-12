dfa1416103250a7d5eda721af5393b39abf35209
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-04-12 12:03:27 +0900

    Update SignIn Page story

---
+++ src/pageContainer/SignInPage/index.stories.tsx
@@ -0,0 +1,12 @@
+import SingInPage from '.';
+
+import { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  title: 'SignInPage',
+  component: SingInPage,
+} as Meta<typeof SingInPage>;
+
+type Story = StoryObj<typeof SingInPage>;
+
+export const Primary: Story = {};
