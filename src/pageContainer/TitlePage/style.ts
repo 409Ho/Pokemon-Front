beb4e93fa9c93cc5cb601500b9a22d017388567a
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-04-12 10:23:28 +0900

    Update TitlePage Style

---
+++ src/pageContainer/TitlePage/style.ts
@@ -0,0 +1,38 @@
+import styled from '@emotion/styled';
+
+export const PageWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export const ContentWrapper = styled.div`
+  width: 600px;
+  height: 100%;
+  background-color: red;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+`;
+
+export const TitlePageContent = styled.div`
+  flex: 1;
+  background-color: #ff0000;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const TItleImageWrapper = styled.div`
+  width: 439px;
+  height: 400px;
+  position: relative;
+  margin: 13.5625rem 4rem 9.5625rem 6.0625rem;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.875rem;
+  margin: 0 auto;
+`;
