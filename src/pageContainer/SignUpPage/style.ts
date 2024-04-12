fc4f66f09376881623ceffbe73253ab7c582803f
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-04-12 11:42:27 +0900

    Add SignUp Page style

---
+++ src/pageContainer/SignUpPage/style.ts
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled';
+
+export const SignUpPageWrapper = styled.div`
+  margin: 0 auto;
+  width: 600px;
+  height: 100vh;
+  background-color: #ff0000;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ContentWrapper = styled.div`
+  flex: 1;
+  margin: 0 auto;
+`;
+
+export const InputWrapper = styled.div`
+  margin: 10.625rem 0 12.5rem 0;
+  display: flex;
+  flex-direction: column;
+  gap: 0.9375rem;
+`;
