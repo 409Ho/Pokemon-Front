4a20c30d1e46e4bf1fb74b0714e480b97b7c21a5
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-04-12 11:42:08 +0900

    Add SignUpPage story

---
+++ src/pageContainer/SignUpPage/index.stories.tsx
@@ -0,0 +1,12 @@
+import SignUpPage from '.';
+
+import { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  title: 'SignUpPage',
+  component: SignUpPage,
+} as Meta<typeof SignUpPage>;
+
+type Story = StoryObj<typeof SignUpPage>;
+
+export const Primary: Story = {};
