1023cfc56d274ee3b5a7ac4c2f2dc88d2e9e5fb3
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-04-25 20:31:47 +0900

    Update sizes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -15,7 +15,7 @@ const Header: React.FC<HeaderProps> = ({ handleModal }) => (
   <S.HeaderWrapper>
     <S.ContentWrapper>
       <S.LogoWrapper>
-        <Image src={pokeLogo} alt="로고" fill />
+        <Image src={pokeLogo} alt="로고" fill sizes="13.125rem 2.5625rem" />
       </S.LogoWrapper>
       <S.IconWrapper onClick={handleModal}>
         <I.HamburgerIcon />
