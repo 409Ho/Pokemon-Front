df2eac1c06a73d0dac61eaacedecaa3bd2e257ff
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-04-26 12:19:37 +0900

    Update Storybook

---
+++ src/pageContainer/PokemonInfoPage/index.stories.tsx
@@ -0,0 +1,12 @@
+import PokemonInfoPage from '.';
+
+import { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  title: 'PokemonInfoPage',
+  component: PokemonInfoPage,
+} as Meta<typeof PokemonInfoPage>;
+
+type Story = StoryObj<typeof PokemonInfoPage>;
+
+export const Primary: Story = {};
