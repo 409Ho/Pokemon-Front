e2556df39da14444518e6eedf2ebc8a1936de52b
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-04-26 12:17:13 +0900

    Add Arrow Icon

---
+++ src/asset/svg/ArrowIcon.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+export const ArrowIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="2.25rem"
+    height="2.25rem"
+    viewBox="0 0 36 36"
+    fill="none"
+  >
+    <path
+      d="M23.625 29.25L12.375 18L23.625 6.75"
+      stroke="white"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
