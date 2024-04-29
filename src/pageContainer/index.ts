06204f34f288e067a123cccb2053139468c1d70a
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-04-29 09:30:55 +0900

    Update export

--- src/pageContainer/index.ts
+++ src/pageContainer/index.ts
@@ -3,3 +3,4 @@ export { default as SignUpPage } from './SignUpPage';
 export { default as SignInPage } from './SignInPage';
 export { default as PokemonBookPage } from './PokemonBookPage';
 export { default as PokemonInfoPage } from './PokemonInfoPage';
+export { default as MyPage } from './MyPage';
