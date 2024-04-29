b5e8d54595937876e7ce74515e82d2c2d857e6e4
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-04-29 13:51:26 +0900

    Update type

--- src/components/RankingCard/index.tsx
+++ src/components/RankingCard/index.tsx
@@ -2,40 +2,29 @@
 
 import Image from 'next/image';
 
+import { RankingCardPros } from '@/types';
+
 import * as S from './style';
 import * as I from '@/asset';
 
-interface RankingCardPros {
-  isKing?: boolean;
-  ranking: number;
-  profileImgUrl: string;
-  catchCount: string;
-  userId: string;
-}
-
-const RankingCard: React.FC<RankingCardPros> = ({
-  isKing,
-  ranking,
-  profileImgUrl,
-  catchCount,
-  userId,
-}) => (
+const RankingCard: React.FC<RankingCardPros> = ({ data }) => (
   <S.RankingCardWrapper>
-    <S.Ranking>{ranking}</S.Ranking>
+    <S.Ranking>{data.ranking}</S.Ranking>
     <S.CardContainer>
       <S.ProfileWrapper>
         <S.ProfileImgWrapper>
           <Image
-            src={profileImgUrl}
+            src={data.profileImgUrl}
             alt="프로필 사진"
             fill
             sizes="4.75rem 4.75rem"
+            unoptimized
           />
         </S.ProfileImgWrapper>
-        <S.UserId>{userId}</S.UserId>
-        <S.CatchTitle>{catchCount}마리 포획</S.CatchTitle>
+        <S.UserId>{data.userId}</S.UserId>
+        <S.CatchTitle>{data.catchCount}마리 포획</S.CatchTitle>
       </S.ProfileWrapper>
-      {isKing && <I.CrownIcon />}
+      {data.isKing && <I.CrownIcon />}
     </S.CardContainer>
   </S.RankingCardWrapper>
 );
