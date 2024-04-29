251950b18b4365dd469714be9e4851df9c96122c
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-04-29 13:51:34 +0900

    Add data

---
+++ src/Data/RankingData.ts
@@ -0,0 +1,184 @@
+import { RankingCardPros } from '@/types';
+
+export const RankingArr: RankingCardPros[] = [
+  {
+    data: {
+      isKing: true,
+      ranking: 1,
+      profileImgUrl:
+        'https://i.namu.wiki/i/2NdOwf3Hy-AoVppNz4JhPULzn7lcsoMuDErOpLt514UcIQyMbwjGxsSGG8wRToB7leZbtv-DxJxWSxdZeD57Aw.webp',
+      catchCount: '333',
+      userId: '덱스',
+    },
+  },
+  {
+    data: {
+      isKing: false,
+      ranking: 2,
+      profileImgUrl:
+        'https://i.namu.wiki/i/2NdOwf3Hy-AoVppNz4JhPULzn7lcsoMuDErOpLt514UcIQyMbwjGxsSGG8wRToB7leZbtv-DxJxWSxdZeD57Aw.webp',
+      catchCount: '333',
+      userId: '덱스',
+    },
+  },
+  {
+    data: {
+      isKing: false,
+      ranking: 3,
+      profileImgUrl:
+        'https://i.namu.wiki/i/2NdOwf3Hy-AoVppNz4JhPULzn7lcsoMuDErOpLt514UcIQyMbwjGxsSGG8wRToB7leZbtv-DxJxWSxdZeD57Aw.webp',
+      catchCount: '333',
+      userId: '덱스',
+    },
+  },
+  {
+    data: {
+      isKing: false,
+      ranking: 4,
+      profileImgUrl:
+        'https://i.namu.wiki/i/2NdOwf3Hy-AoVppNz4JhPULzn7lcsoMuDErOpLt514UcIQyMbwjGxsSGG8wRToB7leZbtv-DxJxWSxdZeD57Aw.webp',
+      catchCount: '333',
+      userId: '덱스',
+    },
+  },
+  {
+    data: {
+      isKing: false,
+      ranking: 5,
+      profileImgUrl:
+        'https://i.namu.wiki/i/2NdOwf3Hy-AoVppNz4JhPULzn7lcsoMuDErOpLt514UcIQyMbwjGxsSGG8wRToB7leZbtv-DxJxWSxdZeD57Aw.webp',
+      catchCount: '333',
+      userId: '덱스',
+    },
+  },
+  {
+    data: {
+      isKing: false,
+      ranking: 6,
+      profileImgUrl:
+        'https://i.namu.wiki/i/2NdOwf3Hy-AoVppNz4JhPULzn7lcsoMuDErOpLt514UcIQyMbwjGxsSGG8wRToB7leZbtv-DxJxWSxdZeD57Aw.webp',
+      catchCount: '333',
+      userId: '덱스',
+    },
+  },
+  {
+    data: {
+      isKing: false,
+      ranking: 7,
+      profileImgUrl:
+        'https://i.namu.wiki/i/2NdOwf3Hy-AoVppNz4JhPULzn7lcsoMuDErOpLt514UcIQyMbwjGxsSGG8wRToB7leZbtv-DxJxWSxdZeD57Aw.webp',
+      catchCount: '333',
+      userId: '덱스',
+    },
+  },
+  {
+    data: {
+      isKing: false,
+      ranking: 8,
+      profileImgUrl:
+        'https://i.namu.wiki/i/2NdOwf3Hy-AoVppNz4JhPULzn7lcsoMuDErOpLt514UcIQyMbwjGxsSGG8wRToB7leZbtv-DxJxWSxdZeD57Aw.webp',
+      catchCount: '333',
+      userId: '덱스',
+    },
+  },
+  {
+    data: {
+      isKing: false,
+      ranking: 9,
+      profileImgUrl:
+        'https://i.namu.wiki/i/2NdOwf3Hy-AoVppNz4JhPULzn7lcsoMuDErOpLt514UcIQyMbwjGxsSGG8wRToB7leZbtv-DxJxWSxdZeD57Aw.webp',
+      catchCount: '333',
+      userId: '덱스',
+    },
+  },
+  {
+    data: {
+      isKing: false,
+      ranking: 10,
+      profileImgUrl:
+        'https://i.namu.wiki/i/2NdOwf3Hy-AoVppNz4JhPULzn7lcsoMuDErOpLt514UcIQyMbwjGxsSGG8wRToB7leZbtv-DxJxWSxdZeD57Aw.webp',
+      catchCount: '333',
+      userId: '덱스',
+    },
+  },
+  {
+    data: {
+      isKing: false,
+      ranking: 11,
+      profileImgUrl:
+        'https://i.namu.wiki/i/2NdOwf3Hy-AoVppNz4JhPULzn7lcsoMuDErOpLt514UcIQyMbwjGxsSGG8wRToB7leZbtv-DxJxWSxdZeD57Aw.webp',
+      catchCount: '333',
+      userId: '덱스',
+    },
+  },
+  {
+    data: {
+      isKing: false,
+      ranking: 12,
+      profileImgUrl:
+        'https://i.namu.wiki/i/2NdOwf3Hy-AoVppNz4JhPULzn7lcsoMuDErOpLt514UcIQyMbwjGxsSGG8wRToB7leZbtv-DxJxWSxdZeD57Aw.webp',
+      catchCount: '333',
+      userId: '덱스',
+    },
+  },
+  {
+    data: {
+      isKing: false,
+      ranking: 13,
+      profileImgUrl:
+        'https://i.namu.wiki/i/2NdOwf3Hy-AoVppNz4JhPULzn7lcsoMuDErOpLt514UcIQyMbwjGxsSGG8wRToB7leZbtv-DxJxWSxdZeD57Aw.webp',
+      catchCount: '333',
+      userId: '덱스',
+    },
+  },
+  {
+    data: {
+      isKing: false,
+      ranking: 14,
+      profileImgUrl:
+        'https://i.namu.wiki/i/2NdOwf3Hy-AoVppNz4JhPULzn7lcsoMuDErOpLt514UcIQyMbwjGxsSGG8wRToB7leZbtv-DxJxWSxdZeD57Aw.webp',
+      catchCount: '333',
+      userId: '덱스',
+    },
+  },
+  {
+    data: {
+      isKing: false,
+      ranking: 15,
+      profileImgUrl:
+        'https://i.namu.wiki/i/2NdOwf3Hy-AoVppNz4JhPULzn7lcsoMuDErOpLt514UcIQyMbwjGxsSGG8wRToB7leZbtv-DxJxWSxdZeD57Aw.webp',
+      catchCount: '333',
+      userId: '덱스',
+    },
+  },
+  {
+    data: {
+      isKing: false,
+      ranking: 16,
+      profileImgUrl:
+        'https://i.namu.wiki/i/2NdOwf3Hy-AoVppNz4JhPULzn7lcsoMuDErOpLt514UcIQyMbwjGxsSGG8wRToB7leZbtv-DxJxWSxdZeD57Aw.webp',
+      catchCount: '333',
+      userId: '덱스',
+    },
+  },
+  {
+    data: {
+      isKing: false,
+      ranking: 17,
+      profileImgUrl:
+        'https://i.namu.wiki/i/2NdOwf3Hy-AoVppNz4JhPULzn7lcsoMuDErOpLt514UcIQyMbwjGxsSGG8wRToB7leZbtv-DxJxWSxdZeD57Aw.webp',
+      catchCount: '333',
+      userId: '덱스',
+    },
+  },
+  {
+    data: {
+      isKing: false,
+      ranking: 18,
+      profileImgUrl:
+        'https://i.namu.wiki/i/2NdOwf3Hy-AoVppNz4JhPULzn7lcsoMuDErOpLt514UcIQyMbwjGxsSGG8wRToB7leZbtv-DxJxWSxdZeD57Aw.webp',
+      catchCount: '333',
+      userId: '덱스',
+    },
+  },
+];
