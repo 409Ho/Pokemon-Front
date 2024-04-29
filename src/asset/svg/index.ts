0660e10e1b58ffe138e59099e08e9a121e616f6b
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-04-29 09:30:44 +0900

    Add Icon export

--- src/asset/svg/index.ts
+++ src/asset/svg/index.ts
@@ -3,3 +3,6 @@ export * from './CrownIcon';
 export * from './PokemonType';
 export * from './FilterIcon';
 export * from './ArrowIcon';
+export * from './PenIcon';
+export * from './BookIcon';
+export * from './ImgIcon';
