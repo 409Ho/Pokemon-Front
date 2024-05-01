94e3d7cfb5e7200231dc9a87365c5c110a0dc029
이정우 <103944346+handsomejeongwoo@users.noreply.github.com>
2024-05-01 13:45:11 +0900

    Update style

--- src/pageContainer/MainPage/style.ts
+++ src/pageContainer/MainPage/style.ts
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 export const PageWrapper = styled.div`
   width: 37.5rem;
-  height: 100%;
+  height: 100vh;
   margin: 0 auto;
   background-color: #ffffff;
   display: flex;
